80de362fcb8b108377f6321b34cb1e65de856ec2
John <johnprogrammer2005@gmail.com>
2018-01-23 15:05:00 +0800

    initial commit

---
+++ test/Solution.java
@@ -0,0 +1,18 @@
+
+
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 6;
+		int z = 11;
+		int[] arr = new int[] {x, y, z};
+		System.out.println(arr);
+		for (int cur : arr) {
+			System.out.println(cur);
+		}
+    }
+
+}
+
